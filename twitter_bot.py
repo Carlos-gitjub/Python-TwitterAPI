e99e38b8f65f3d05d81449e9d81b678f5cde8e2d
carlos <cantonplk@gmail.com>
2021-04-26 17:03:55 +0100

    updates

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,15 +3,15 @@ import tweepy
 consumer_key = 'kqXh5PAmscY9CRVBlWP9Fss7V'
 consumer_secret = 'J5oowaDwLlX8ChL2IwKCetnvWrDXnMBlYRHpAd5WPrAmusQ9DJ'
 
-
-
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
 api = tweepy.API(auth)
+
+user = api.get_user('@solrakbot')
+
 for tweet in tweepy.Cursor(api.search, q='tweet 1 kljsfdjklfsda').items(10):
     print(tweet.text)
 
-user = api.get_user('@Solrak45237861')
-print(user.screen_name)
-print(user.followers_count)
+print('Nombre usuario: ' + user.screen_name)
+print('Numero de seguidores: ' + str(user.followers_count))
 
