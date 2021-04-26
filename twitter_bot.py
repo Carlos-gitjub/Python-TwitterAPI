81d9e12184661f2d7cd3211d71e84c7c5ee402a3
carlos <cantonplk@gmail.com>
2021-04-26 13:53:48 +0100

    updates

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,15 +1,10 @@
 import tweepy
 
-print('this is my twitter bot')
+consumer_key = 'kqXh5PAmscY9CRVBlWP9Fss7V'
+consumer_secret = 'J5oowaDwLlX8ChL2IwKCetnvWrDXnMBlYRHpAd5WPrAmusQ9DJ'
 
-CONSUMER_KEY = 'kqXh5PAmscY9CRVBlWP9Fss7V'                          # 'dxRifUFHDsJFMG1ZF4R0IlHZo'
-CONSUMER_SECRET = 'J5oowaDwLlX8ChL2IwKCetnvWrDXnMBlYRHpAd5WPrAmusQ9DJ' #'9llzT36hmcDRogO3tKTunnEdHTAWjVRX5du4ewENR4dTqUF47n'
-ACCESS_KEY = 'AAAAAAAAAAAAAAAAAAAAAKG6OwEAAAAAaGLczQiWHKWhy6DQPTQhwV1WPXo%3D5T4q'
-ACCESS_SECRET = 'NxHyei5QUjzsVFbqYV5BXLfh38Mez6BODHPd64r8kHFccS'
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 
 api = tweepy.API(auth)
-
-api.update_status('Twitter reporting in live')
+for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
+    print(tweet.text)
