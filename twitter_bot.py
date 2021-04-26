5577a1d827d9154714a440315d4c15e32cd2a3ba
carlos <cantonplk@gmail.com>
2021-04-26 18:18:12 +0100

    update

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,14 +2,19 @@ import tweepy
 
 print('this is my twitter bot')
 
-CONSUMER_KEY = 'kqXh5PAmscY9CRVBlWP9Fss7V'                          # 'dxRifUFHDsJFMG1ZF4R0IlHZo'
-CONSUMER_SECRET = 'J5oowaDwLlX8ChL2IwKCetnvWrDXnMBlYRHpAd5WPrAmusQ9DJ' #'9llzT36hmcDRogO3tKTunnEdHTAWjVRX5du4ewENR4dTqUF47n'
+#Passwords
+CONSUMER_KEY = 'kqXh5PAmscY9CRVBlWP9Fss7V'                          
+CONSUMER_SECRET = 'J5oowaDwLlX8ChL2IwKCetnvWrDXnMBlYRHpAd5WPrAmusQ9DJ' 
+
 ACCESS_KEY = 'AAAAAAAAAAAAAAAAAAAAAKG6OwEAAAAAaGLczQiWHKWhy6DQPTQhwV1WPXo%3D5T4q'
 ACCESS_SECRET = 'NxHyei5QUjzsVFbqYV5BXLfh38Mez6BODHPd64r8kHFccS'
 
+#Nos logeamos en la API con nuestra cuenta de Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
+#Creamos la variable para acceder a la API
 api = tweepy.API(auth)
 
-api.update_status('Twitter reporting in live')
+#Ejemplo: tweeteamos algo
+api.update_status('hola cocacola')
