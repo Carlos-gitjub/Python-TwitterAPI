c81bf6c54c4d9f2a71641824a1f2d8ee1cf3d1bf
Carlos-gitjub <79362942+Carlos-gitjub@users.noreply.github.com>
2021-05-26 20:53:25 +0100

    Update twitter_bot.py

--- twitter_bot.py
+++ twitter_bot.py
@@ -3,13 +3,13 @@ import tweepy
 print('this is my twitter bot')
 
 #Passwords
-CONSUMER_KEY = '86bUDs9JMdtbyvI1dqKOW1Z1G'                          
-CONSUMER_SECRET = 'wlCzldSmGE54TmxXPFNW3aNPAcYdjk4nKIX0HPpXDnEOByVQ0F' 
+CONSUMER_KEY = 'jkldsfalkfdalkfdjs'                          
+CONSUMER_SECRET = 'klsdfalkjsda' 
 
-BEARER_TOKEN= 'AAAAAAAAAAAAAAAAAAAAAKG6OwEAAAAAxXoSj4mQ4ZfoE8ZFnxCg9%2FgcTIg%3DkcPKHT8j4NVMBK7McfCymdoclNY9T0XYembra7OrtrdtwDaW4G'  #No se usa. La dejo por aquí por si acaso
+BEARER_TOKEN= 'fsdaffdsfasdfafds'  #No se usa. La dejo por aquí por si acaso
 
-ACCESS_KEY = '1385643890762518532-4ilMLXwsuzFDRR7jJtmngMIbkh2b7W'  
-ACCESS_SECRET = 'Cc7gLGRn4nxAvFFOPTmDbkZ1the1jAnHmHtG6ZX3KFeRS'
+ACCESS_KEY = 'fdsafdsafdasdfsdfsdfdsfadfsafd'  
+ACCESS_SECRET = 'dsafdsafsdfasdfasdfS'
 
 #Nos logeamos en la API con nuestra cuenta de Twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
